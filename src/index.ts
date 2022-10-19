dc1b46dafd49bd2fbbd8d851de8066cc30bdd1f8
balaganapathyparthiban <balaganapathy.parthiban@gmail.com>
2022-10-19 20:56:28 +0530

    Refactor code and changed SSE event logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,60 @@
 import { v4 as uuidv4 } from 'uuid'
 
-const ID = uuidv4()
-
-export const browsernet = () => {
-    new BrowsernetRTC()
+/**
+ * INTERFACES
+ */
+interface BrowserNetOptions {
+    sseBaseURL: string
 }
 
+interface BrowserNetReturn {
+    close: () => void
+}
+
+interface Payload {
+    type: string,
+    payload: {
+        [key: string]: any
+    }
+}
+
+/**
+ * VARIABLES
+ */
+const ID = uuidv4()
+const CONNECTED_ID: string[] = []
+const CONNECTIONS: BrowsernetRTC[] = []
+const TYPE_NEW_CONNECTION = 'NEW_CONNECTION'
+const TYPE_NO_OFFER = 'NO_OFFER'
+const TYPE_OFFER = 'OFFER'
+const TYPE_ANWSER = 'ANWSER'
+const TYPE_ICE_CANDIDATE = 'ICE_CANDIDATE'
+let OPTIONS: BrowserNetOptions = {
+    sseBaseURL: ''
+}
+
+/**
+ * 
+ * @param options 
+ * @returns 
+ */
+const browsernet = (options: BrowserNetOptions = OPTIONS): BrowserNetReturn => {
+    OPTIONS = { ...options }
+
+    CONNECTIONS.push(new BrowsernetRTC())
+
+    return {
+        close: () => {
+            CONNECTIONS.forEach(connection => {
+                connection.close()
+            })
+        }
+    }
+}
+
+/**
+ * BROWSERNET RTC IMPLEMENTATION 
+ */
 class BrowsernetRTC {
     private configuration: RTCConfiguration = {
         iceServers: [
@@ -45,7 +94,13 @@ class BrowsernetRTC {
     private dataChannel: RTCDataChannel
     private offer: RTCSessionDescriptionInit | undefined
     private iceCandidate: RTCIceCandidate[] = []
+    private connectedTo: string | undefined
+    private sseSession: EventSource | undefined
 
+    /**
+     * 
+     * @param iceServers 
+     */
     constructor(iceServers: RTCIceServer[] = []) {
         console.log(ID)
         this.configuration.iceServers = [...this.configuration.iceServers!, ...iceServers]
@@ -53,45 +108,111 @@ class BrowsernetRTC {
         this.peerConnection = new RTCPeerConnection(this.configuration)
         this.dataChannel = this.peerConnection.createDataChannel(new Date().getTime().toString())
 
-        this.createOffer()
+        this.initSSESession()
+
+        /**
+         * Events
+         */
+        this.dataChannel.addEventListener('open', event => {
+            console.log('data channel open')
+        });
+
+        this.peerConnection.addEventListener('connectionstatechange', event => {
+            console.log(this.peerConnection.connectionState)
+        });
     }
 
-    private createOffer() {
-        this.peerConnection.createOffer()!
-            .then(offer => {
-                this.offer = offer
-                this.peerConnection.setLocalDescription(this.offer)
-                this.getIceCandidate()
-            })
+    /**
+     * 
+     */
+    private initSSESession() {
+        const payloadString = JSON.stringify({
+            type: TYPE_NEW_CONNECTION,
+            payload: {
+                id: ID,
+                connectedIDS: CONNECTED_ID
+            }
+        })
+
+        this.sseSession = new EventSource(`${OPTIONS.sseBaseURL}/browsernet/sse?payload=${payloadString}`)
+        this.sseSession.addEventListener('message', (event: MessageEvent<any>) => this.sseMessageEvent(event))
     }
 
-    private getIceCandidate() {
+    /**
+     * 
+     * @param event 
+     * @returns 
+     */
+    private sseMessageEvent(event: MessageEvent<any>) {
+        if (event.data) {
+            const payloadParsed: Payload = JSON.parse(event?.data)
+            if (!payloadParsed?.type) return
+
+            switch (payloadParsed.type) {
+                case TYPE_NO_OFFER: {
+                    alert('NO OFFER')
+                    break
+                }
+                case TYPE_OFFER: {
+                    break
+                }
+                case TYPE_ANWSER: {
+                    break
+                }
+                case TYPE_ICE_CANDIDATE: {
+                    break
+                }
+                default: {
+                    break
+                }
+            }
+            // await this.peerConnection.setRemoteDescription(new RTCSessionDescription(payloadParsed?.offer))
+            // await this.peerConnection.addIceCandidate(payloadParsed?.iceCandidate[0])
+
+            // CONNECTED_ID.push(payloadParsed?.id)
+        }
+    }
+
+    /**
+     * 
+     */
+    private generateIceCandidate() {
         this.peerConnection.addEventListener('icecandidate', event => {
             if (event.candidate) {
                 this.iceCandidate.push(event.candidate)
+                this.syncWithSSESession(
+                    JSON.stringify({
+                        type: TYPE_ICE_CANDIDATE,
+                        payload: {
+                            id: ID,
+                            connectedTo: this.connectedTo,
+                            iceCandidate: event.candidate,
+                        }
+                    })
+                )
             }
         });
-        this.createSSE()
     }
 
-    private createSSE() {
-        setTimeout(() => {
-            const payload = JSON.stringify({
-                id: ID,
-                offer: this.offer,
-                iceCandidate: this.iceCandidate,
-                deps: []
-            })
-            const sse = new EventSource(`http://localhost:5000/browsernet/sse?payload=${payload}`)
-            sse.addEventListener('message', this.sseMessageListener)
-        }, 10000)
+    /**
+     * 
+     * @param payload 
+     * @returns 
+     */
+    private syncWithSSESession(payload: string) {
+        return fetch(`${OPTIONS.sseBaseURL}/browsernet/session?payload=${payload}`)
+            .then((response) => response.json())
     }
-    
-    private sseMessageListener(event: MessageEvent<any>) {
-        console.log(event?.data)
-        if (event.data) {
-            /* @TODO logic */
-        }
 
+    /**
+     * 
+     */
+    public close() {
+        this.sseSession?.close()
     }
 }
+
+/**
+ * EXPORT DEFAULT BROWSERNET
+ */
+export default browsernet
